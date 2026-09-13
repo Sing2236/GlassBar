04f26776d33e0b4721309bec6dd6b2127a9e510e
Sing2236 <Ethanhuynh365@gmail.com>
2026-09-13 17:34:39 -0500

    feat: animate rain across the site preview

--- website/playwright.config.js
+++ website/playwright.config.js
@@ -1,14 +1,15 @@
 const { defineConfig, devices } = require("@playwright/test");
+const liveBaseUrl = process.env.PLAYWRIGHT_BASE_URL;
 
 module.exports = defineConfig({
   testDir: "./tests",
   outputDir: "./test-results",
   reporter: "line",
   use: {
-    baseURL: "http://127.0.0.1:4173",
+    baseURL: liveBaseUrl || "http://127.0.0.1:4173",
     trace: "retain-on-failure"
   },
-  webServer: {
+  webServer: liveBaseUrl ? undefined : {
     command: "npx http-server . -p 4173 -c-1",
     url: "http://127.0.0.1:4173",
     reuseExistingServer: true
